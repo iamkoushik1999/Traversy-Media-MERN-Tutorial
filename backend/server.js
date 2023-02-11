a89e41c793157e3d74d9f6e65fd98ba8337dd3c9
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-02-11 13:11:37 +0530

    Routes FIle

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,6 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/api/goals", (req, res) =>
-  res.status(200).json({ message: "Get Goals" })
-);
+app.use("/api/goals", require("./routes/goalRoutes"));
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));
