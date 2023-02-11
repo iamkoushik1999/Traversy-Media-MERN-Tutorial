3dddffd20e2e53093dea9c3310fe8a49749c137b
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-02-11 13:43:55 +0530

    Express Async Handler

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require("express-async-handler");
+
 // desc    Get Goals
 // route   Get api/goals
 // access  Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get Goals" });
-};
+});
 
 // desc    Set Goals
 // route   Post api/goals
 // access  Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text feild");
   }
 
   res.status(200).json({ message: "Set Goal" });
-};
+});
 
 // desc    Update Goals
 // route   Put api/goals
 // access  Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update Goal ${req.params.id}` });
-};
+});
 
 // desc    Delete Goals
 // route   Delete api/goals
 // access  Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete Goal ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
