95ea264612ba2de03e2b9304b0ebf3b7976a493a
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-02-11 13:01:29 +0530

    First Route Set

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/api/goals", (req, res) =>
+  res.status(200).json({ message: "Get Goals" })
+);
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));
