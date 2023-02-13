45a4a114642353966ea605cf333a1458d7ad9430
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-02-13 22:14:49 +0530

    Register User

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,26 +1,45 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
 // desc    Register New User
 // route   Post api/users
 // access  Public
 
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  // check user exist
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
   res.json({ message: "register user" });
-};
+});
 
 // desc    Authenticate User
 // route   Post api/users/login
 // access  Public
 
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.json({ message: "Login user" });
-};
+});
 
 // desc    Get User Data
 // route   Get api/users
 // access  Private
 
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "User data display" });
-};
+});
 
 module.exports = {
   registerUser,
