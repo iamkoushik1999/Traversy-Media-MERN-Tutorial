539d7d9ab77409125ab06e624ad001ff961d257a
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-02-13 22:20:51 +0530

    Hash Password

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -22,7 +22,27 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("User Already Exists");
   }
 
-  res.json({ message: "register user" });
+  // Hash Password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user Data");
+  }
 });
 
 // desc    Authenticate User
