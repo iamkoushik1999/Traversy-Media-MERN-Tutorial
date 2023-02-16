701bb03f933087f1654bd954a4fe8171c8f3d2a1
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-02-16 21:06:26 +0530

    better comments

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,25 @@ const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 
+// Connect Database
 connectDB();
 
+// Start express
 const app = express();
 const port = process.env.PORT || 5000;
 
+// JSON format
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
+//Error Handler
 app.use(errorHandler);
 
+// Confirmation on server
 app.listen(port, () => {
   console.log(`Server started on port ${port}!`);
 });
