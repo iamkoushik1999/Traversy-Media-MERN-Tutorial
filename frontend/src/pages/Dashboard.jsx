8fdccb1035afe982ff012e42ffe74e7bac112b09
KoushikDutta1999 <iamkoushik1999@gmail.com>
2023-02-18 13:46:48 +0530

    Display Goals

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useNavigate } from "react-router-dom";
 import { useSelector,useDispatch } from "react-redux";
 import GoalForm from "../components/GoalForm";
+import GoalItem from "../components/GoalItem";
 import Spinner from "../components/Spinner";
 import { getGoals, reset } from "../features/goals/goalSlice";
 
@@ -41,6 +42,17 @@ function Dashboard() {
         <p>Goals Dashboard</p>
       </section>
       <GoalForm />
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals yet</h3>
+        )}
+      </section>
     </>
   )
 }
